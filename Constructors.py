dba37f90177c3b43eba13e32ba6361485856649c
xstopher <Jayden.odhiambo8@gmail.com>
2026-03-20 16:09:58 +0300

    updated Constructors

---
+++ Constructors.py
@@ -0,0 +1,9 @@
+class Jordan:
+    def __init__(self, model, colorway):
+        # 1. INITIALIZATION
+        self.model = model
+        self.colorway = colorway
+        print(f"Jordan {self.model} in {self.colorway} is ready!")
+
+# Creating the object 
+my_shoe = Jordan("Jordan 1", "Chicago")
